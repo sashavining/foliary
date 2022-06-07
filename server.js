c622b0e92145774e80dc1daa6769c171090a6f95
sashavining <96848086+sashavining@users.noreply.github.com>
2022-06-06 22:27:58 -0400

    Add CORS module

--- server.js
+++ server.js
@@ -2082,9 +2082,11 @@ const plants = [
 
 const { response } = require('express');
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const PORT = 8000
 
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');  
